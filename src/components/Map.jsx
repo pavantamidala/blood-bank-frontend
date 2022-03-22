df6c96f20dcfb02ce0bded2c486debc9205cecd1
pavantamidala <pavantamidala023@gmail.com>
2022-03-22 20:07:19 +0530

    ftr:removed unnecessary

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,11 +12,11 @@ const center = {
   lat: -3.745,
   lng: -38.523,
 };
-
+console.log(process.env)
 export default function Map() {
   return (
     <div className="mapWrapper">
-      <LoadScript googleMapsApiKey="AIzaSyDDdcrX0rUGZi9kplSBZ7hA-4c0Zjl5E0s">
+      <LoadScript googleMapsApiKey={ process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
           {/* Child components, such as markers, info windows, etc. */}
           <></>
